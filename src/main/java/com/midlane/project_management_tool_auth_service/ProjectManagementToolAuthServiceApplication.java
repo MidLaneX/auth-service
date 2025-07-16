72446bdb4928b760ece6a20f8ebac15c1716f111
Pasindu <pasindusathsara60@gmail.com>
2025-07-16 16:18:44 +0530

    Initial commit

---
+++ src/main/java/com/midlane/project_management_tool_auth_service/ProjectManagementToolAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.midlane.project_management_tool_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementToolAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midlane/project_management_tool_auth_service/ProjectManagementToolAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midlane.project_management_tool_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementToolAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
