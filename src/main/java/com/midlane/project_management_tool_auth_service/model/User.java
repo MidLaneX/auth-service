242229f5a89df40b556ee6b410d040150ce30235
Pasindu <pasindusathsara60@gmail.com>
2025-07-17 07:33:37 +0530

    User entity added

---
+++ src/main/java/com/midlane/project_management_tool_auth_service/model/User.java
@@ -0,0 +1,39 @@
+package com.midlane.project_management_tool_auth_service.model;
+
+import lombok.*;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", updatable = false, nullable = false)
+    private Long userId;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "password_last_changed")
+    private LocalDateTime passwordLastChanged;
+
+    @CreationTimestamp
+    @Column(name = "user_created", nullable = false, updatable = false)
+    private LocalDateTime userCreated;
+
+    @Column(name = "email_last_changed")
+    private LocalDateTime emailLastChanged;
+}
