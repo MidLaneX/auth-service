4d331498ee22ff54a5fffedcc731b716c442bce5
Pasindu <pasindusathsara60@gmail.com>
2025-08-09 10:30:19 +0530

    refactor(all): cleanup

--- src/main/java/com/midlane/project_management_tool_auth_service/config/WebClientConfig.java
+++ src/main/java/com/midlane/project_management_tool_auth_service/config/WebClientConfig.java
@@ -2,6 +2,7 @@ package com.midlane.project_management_tool_auth_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -10,5 +11,10 @@ public class WebClientConfig {
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
